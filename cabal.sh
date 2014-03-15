a4b824d0a33fac5edd6bf1e57c7d633e2b6f8e1c
Bryan Gardiner <bog@khumba.net>
2014-03-15 15:56:53 -0700

    Apply the AGPLv3 to Goatee.

--- cabal.sh
+++ cabal.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# This file is part of Goatee.
+#
+# Copyright 2014 Bryan Gardiner
+#
+# Goatee is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Goatee is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Goatee.  If not, see <http://www.gnu.org/licenses/>.
+
 CABAL="$(which cabal 2>/dev/null)"
 if test $? -ne 0; then
     ghc Setup || { echo "Couldn't build Setup.hs, aborting."; exit 1; }
--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,20 @@
+-- This file is part of Goatee.
+--
+-- Copyright 2014 Bryan Gardiner
+--
+-- Goatee is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Goatee is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with Goatee.  If not, see <http://www.gnu.org/licenses/>.
+
 -- | Main module, controls application start-up.
 module Main (main) where
 
