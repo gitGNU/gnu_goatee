a1743278ed8b096eed33aef729756344e087ad52
Bryan Gardiner <bog@khumba.net>
2014-03-27 23:34:10 -0700

    Light CoordList clean-up.
    - Adds coord1 for constructing a singleton CoordList and documents the
      constructor functions.
    
    - Makes the CoordList data constructor private since coords' does the job.
    
    - Moves the CoordList monoid instance into the parser, since it's not
      fit for use outside of parsing (it doesn't dedup).
    
    - Changes the buildCoordList input from a [[Bool]] grid to [Coord] which is
      a more typical input format.

--- src/Khumba/Goatee/Sgf/Types.hs
+++ src/Khumba/Goatee/Sgf/Types.hs
@@ -17,7 +17,7 @@
 
 -- | Data types for property values used in SGF game trees.
 module Khumba.Goatee.Sgf.Types (
-  Coord, CoordList(..), coords, coords',
+  Coord, CoordList(coordListSingles, coordListRects), coord1, coords, coords',
   emptyCoordList, expandCoordList, buildCoordList,
   RealValue,
   Stringlike(..),
@@ -36,41 +36,55 @@ module Khumba.Goatee.Sgf.Types (
 import Data.Char (isSpace)
 import Data.Function (on)
 import Data.List (delete, groupBy, partition, sort)
-import Data.Monoid
 
 -- | A coordinate on a Go board.  @(0, 0)@ refers to the upper-left corner of
 -- the board.  The first component is the horizontal position; the second
 -- component is the vertical position.
 type Coord = (Int, Int)
 
--- | Allows for compact representation of a list of coordinates.  Convert to a
--- list with 'expandCoordList'.
+-- | A structure for compact representation of a list of coordinates.  Contains
+-- a list of individual points, as well as a list of rectangles of points
+-- denoted by an ordered pair of the upper-left point and the lower-right point.
+-- The union of the single points and points contained within rectangles make up
+-- all of the points a @CoordList@ represents.  There is no rule saying that
+-- adjacent points have to be grouped into rectangles; it's perfectly valid
+-- (although possibly inefficient) to never use rectangles.
+--
+-- For any @CoordList@, all of the following hold:
+--
+-- 1. Any point may be specified at most once, either in the singles list or in
+-- a single rectangle.
+--
+-- 2. For a rectangle @((x0,y0), (x1,y1))@, @x0 <= x1@ and @y0 <= y1@ and
+-- @(x0,y0) /= (x1,y1)@ (otherwise the point belongs in the singles list).
 data CoordList = CoordList { coordListSingles :: [Coord]
                            , coordListRects :: [(Coord, Coord)]
                            } deriving (Show)
 
+-- | Equality is based on unordered, set equality of the underlying points.
 instance Eq CoordList where
   (==) = (==) `on` sort . expandCoordList
 
-instance Monoid CoordList where
-  mempty = CoordList { coordListSingles = []
-                     , coordListRects = []
-                     }
+-- | Constructs a 'CoordList' containing a single point.
+coord1 :: Coord -> CoordList
+coord1 xy = CoordList { coordListSingles = [xy]
+                      , coordListRects = []
+                      }
 
-  mappend x y = CoordList { coordListSingles = coordListSingles x ++ coordListSingles y
-                          , coordListRects = coordListRects x ++ coordListRects y
-                          }
-
+-- | Constructs a 'CoordList' containing the given single points.  For rectangle
+-- detection, use 'buildCoordList'.
 coords :: [Coord] -> CoordList
 coords singles = CoordList { coordListSingles = singles
                            , coordListRects = []
                            }
 
+-- | Constructs a 'CoordList' containing the given single points and rectangles.
 coords' :: [Coord] -> [(Coord, Coord)] -> CoordList
 coords' singles rects = CoordList { coordListSingles = singles
                                   , coordListRects = rects
                                   }
 
+-- | A 'CoordList' that contains no points.
 emptyCoordList :: CoordList
 emptyCoordList = CoordList { coordListSingles = []
                            , coordListRects = []
@@ -86,19 +100,10 @@ expandCoordList cl = coordListSingles cl ++
                            []
                            (coordListRects cl)
 
--- | Constructs a 'CoordList' from a grid of booleans.  The rectangles and
--- points in the result represent those booleans that were true.  The order of
--- data in the result is unspecified.
---
--- One possible result is as follows.
---
--- >>> [[False, True, True, False, True],
---      [False, True, True, True, False],
---      [False, True, True, True, False]]
--- CoordList { coordListSingles = [(4,0)],
---             coordListRects = [((1,1),(3,2)),((1,0),(2,0))]
---           }
-buildCoordList :: [[Bool]] -> CoordList
+-- | Constructs a 'CoordList' from a list of 'Coord's, doing some
+-- not-completely-stupid rectangle detection.  The order of data in the result
+-- is unspecified.
+buildCoordList :: [Coord] -> CoordList
 -- This algorithm doesn't generate the smallest result.  For the following
 -- input:
 --
@@ -107,8 +112,16 @@ buildCoordList :: [[Bool]] -> CoordList
 -- T T F F
 --
 -- It will return [ca:da][ab:db][ac:bc] rather than the shorter [ca:db][ab:bc].
-buildCoordList = toCoordList . generateRects 0 [] . buildTruePairs
-  where -- | For each row, converts a list of booleans into a list of pairs
+buildCoordList = toCoordList . generateRects 0 [] . buildTruePairs . toGrid
+  where -- | Constructs a row-major grid of booleans where an coordinate is true
+        -- iff it is in the given list.
+        toGrid :: [Coord] -> [[Bool]]
+        toGrid [] = []
+        toGrid coords = let x1 = maximum $ map fst coords
+                            y1 = maximum $ map snd coords
+                        in [[(x,y) `elem` coords | x <- [0..x1]] | y <- [0..y1]]
+
+        -- | For each row, converts a list of booleans into a list of pairs
         -- where each pair represents a consecutive run of true values.  The
         -- pair indicates the indices of the first and last boolean in each run.
         buildTruePairs :: [[Bool]] -> [[(Int, Int)]]
--- src/Khumba/Goatee/Sgf/TypesTest.hs
+++ src/Khumba/Goatee/Sgf/TypesTest.hs
@@ -32,127 +32,96 @@ tests = testGroup "Khumba.Goatee.Sgf.Types" [
   ]
 
 expandCoordListTests = testGroup "expandCoordList" [
-  testCase "works with an empty CoordList" $
-    [] @=? expandCoordList CoordList {coordListSingles = [], coordListRects = []},
+  testCase "works with an empty CoordList" $ do
+    [] @=? expandCoordList (coords [])
+    [] @=? expandCoordList (coords' [] []),
 
   testCase "works for single points" $ do
-    [(1,2)] @=? expandCoordList CoordList { coordListSingles = [(1,2)]
-                                          , coordListRects = []
-                                          }
-    [(3,4), (1,2)] @=? expandCoordList CoordList { coordListSingles = [(3,4), (1,2)]
-                                                 , coordListRects = []
-                                                 }
+    [(1,2)] @=? expandCoordList (coord1 (1,2))
+    [(3,4), (1,2)] @=? expandCoordList (coords [(3,4), (1,2)])
     let ten = [(i,i) | i <- [1..10]]
-    ten @=? expandCoordList CoordList { coordListSingles = ten
-                                      , coordListRects = []
-                                      },
+    ten @=? expandCoordList (coords ten),
 
   -- TODO Test that a 1x1 rectangle is rejected.
 
   testCase "works for a nx1 rect" $
-    [(5,2), (5,3), (5,4)] @=? expandCoordList CoordList { coordListSingles = []
-                                                        , coordListRects = [((5,2), (5,4))]
-                                                        },
+    [(5,2), (5,3), (5,4)] @=? expandCoordList (coords' [] [((5,2), (5,4))]),
 
   testCase "works for a 1xn rect" $
-    [(1,0), (1,1), (1,2), (1,3)] @=? expandCoordList CoordList { coordListSingles = []
-                                                               , coordListRects = [((1,0), (1,3))]
-                                                               },
+    [(1,0), (1,1), (1,2), (1,3)] @=? expandCoordList (coords' [] [((1,0), (1,3))]),
 
   testCase "works for an mxn rect" $
     [(m,n) | m <- [2..5], n <- [3..7]] @=?
-    expandCoordList CoordList { coordListSingles = []
-                              , coordListRects = [((2,3), (5,7))]
-                              },
+    expandCoordList (coords' [] [((2,3), (5,7))]),
 
   -- TODO Test that x0 > x1 || y0 > y1 is rejected.
 
   testCase "works with multiple rects" $
     [(0,0), (0,1), (0,2), (3,4), (4,4), (5,4)] @=?
-    expandCoordList CoordList { coordListSingles = []
-                              , coordListRects = [((0,0), (0,2)), ((3,4), (5,4))]
-                              },
+    expandCoordList (coords' [] [((0,0), (0,2)), ((3,4), (5,4))]),
 
   testCase "concatenates single points and rects" $
     [(1,1), (0,0), (2,2), (2,3), (2,4)] @=?
-    expandCoordList CoordList { coordListSingles = [(1,1), (0,0)]
-                              , coordListRects = [((2,2), (2,4))]
-                              }
+    expandCoordList (coords' [(1,1), (0,0)] [((2,2), (2,4))])
   ]
 
 buildCoordListTests = testGroup "buildCoordList" [
-  testCase "handles the trivial case []" $ assertSinglesAndRects [] [] [],
-
-  testCase "handles the trivial case [[]]" $ assertSinglesAndRects [] [] [[]],
-
-  testCase "handles all being false (1x1)" $ assertGrid 1 1 [],
-
-  testCase "handles all being false (2x2)" $ assertGrid 2 2 [],
-
-  testCase "handles all being false (4x3)" $ assertGrid 4 3 [],
+  testCase "handles the empty case" $ assertSinglesAndRects [] [] [],
 
   testCase "handles one single" $
-    assertSinglesAndRects [(0,1)] [] $ grid 2 2 [(0,1)],
+    assertSinglesAndRects [(0,1)] [] [(0,1)],
 
   testCase "handles multiple singles" $
-    assertSinglesAndRects [(0,0), (1,1)] [] $ grid 2 2 [(0,0), (1,1)],
+    assertSinglesAndRects [(0,0), (1,1)] [] [(0,0), (1,1)],
 
   testCase "handles a small rect (1x2)" $
-    assertSinglesAndRects [] [((0,1), (0,2))] $ grid 1 3 [(0,1), (0,2)],
+    assertSinglesAndRects [] [((0,1), (0,2))] [(0,1), (0,2)],
 
   testCase "handles a small square (2x2)" $
-    assertSinglesAndRects [] [((1,1), (2,2))] $ grid 4 4 [(x,y) | x <- [1..2], y <- [1..2]],
+    assertSinglesAndRects [] [((1,1), (2,2))] [(x,y) | x <- [1..2], y <- [1..2]],
 
   testCase "handles a larger rect" $
-    assertSinglesAndRects [] [((0,0), (2,4))] $ grid 3 5 [(x,y) | x <- [0..2], y <- [0..4]],
+    assertSinglesAndRects [] [((0,0), (2,4))] [(x,y) | x <- [0..2], y <- [0..4]],
 
   testCase "handles two rects" $
-    assertSinglesAndRects [] [((0,1), (1,2)), ((3,0), (4,1))] $
-    grid 5 3 [(0,1), (1,1), (0,2), (1,2), (3,0), (4,0), (3,1), (4,1)],
+    assertSinglesAndRects [] [((0,1), (1,2)), ((3,0), (4,1))]
+    [(0,1), (1,1), (0,2), (1,2), (3,0), (4,0), (3,1), (4,1)],
 
   testCase "handles rects and singles together" $
-    assertSinglesAndRects [(0,0)] [((1,1), (2,2))] $
-    grid 3 3 [(0,0), (1,1), (1,2), (2,1), (2,2)],
+    assertSinglesAndRects [(0,0)] [((1,1), (2,2))]
+    [(0,0), (1,1), (1,2), (2,1), (2,2)],
 
   testCase "handles five points together" $ do
-    assertGrid 2 3 [(1,0), (0,1), (1,1), (0,2), (1,2)]
-    assertGrid 2 3 [(0,0), (1,0), (0,1), (1,1), (0,2)]
-    assertGrid 3 2 [(0,0), (1,0), (2,0), (0,1), (1,1)]
-    assertGrid 3 2 [(1,0), (2,0), (0,1), (1,1), (2,1)],
+    assertCoords [(1,0), (0,1), (1,1), (0,2), (1,2)]
+    assertCoords [(0,0), (1,0), (0,1), (1,1), (0,2)]
+    assertCoords [(0,0), (1,0), (2,0), (0,1), (1,1)]
+    assertCoords [(1,0), (2,0), (0,1), (1,1), (2,1)],
 
   testCase "handles a more complex case" $
-    assertGrid 5 5 [(3,0), (4,0),
-                    (3,1), (4,1),
-                    (0,2), (1,2), (2,2), (3,2), (4,2),
-                    (0,3), (1,3),
-                    (0,4), (1,4), (3,4), (4,4)]
-  ]
-  where -- | Constructs a grid of booleans with the given coordinates set to
-        -- true.
-        grid :: Int -> Int -> [Coord] -> [[Bool]]
-        grid width height trues = [[(x,y) `elem` trues | x <- [0..width-1]] | y <- [0..height-1]]
+    assertCoords [(3,0), (4,0),
+                  (3,1), (4,1),
+                  (0,2), (1,2), (2,2), (3,2), (4,2),
+                  (0,3), (1,3),
+                  (0,4), (1,4), (3,4), (4,4)],
 
-        -- | Asserts that the given grid of booleans parses to a 'CoordList'
+  testCase "handles a diagonal pattern" $
+    assertCoords [(0,0), (2,0), (4,0),
+                  (1,1), (3,1), (5,1),
+                  (2,2)]
+  ]
+  where -- | Asserts that the given grid of booleans parses to a 'CoordList'
         -- that has the expected single points (not necessarily in the same
         -- order), as well as the expected rectangles (not necessarily in the
         -- same order).
-        assertSinglesAndRects :: [Coord] -> [(Coord, Coord)] -> [[Bool]] -> Assertion
+        assertSinglesAndRects :: [Coord] -> [(Coord, Coord)] -> [Coord] -> Assertion
         assertSinglesAndRects expectedSingles expectedRects input =
           coords' expectedSingles expectedRects @=? buildCoordList input
 
         -- | Asserts that the set of points returned from applying
-        -- 'buildCoordList' to the given grid of booleans is equal to the given
-        -- set of points.
-        assertCoords :: [Coord] -> [[Bool]] -> Assertion
-        assertCoords expectedCoords input =
-          sort expectedCoords @=? sort (expandCoordList $ buildCoordList input)
-
-        -- | Asserts that, on a grid of the given width and height and only the
-        -- given points set to true, that the set of points returned from
-        -- applying 'buildCoordList' to this grid is equal to the set of points
-        -- given as input.
-        assertGrid :: Int -> Int -> [Coord] -> Assertion
-        assertGrid width height trues = assertCoords trues $ grid width height trues
+        -- 'buildCoordList' to the given grid of booleans is equal to
+        -- the given set of points.
+        assertCoords :: [Coord] -> Assertion
+        assertCoords input = sort input @=? sort (expandCoordList $ buildCoordList input)
 
 simpleTextTests = testGroup "SimpleText" [
   testCase "accepts the empty string" $
